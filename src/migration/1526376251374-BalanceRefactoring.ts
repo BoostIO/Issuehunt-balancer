9d0ca7ad142f5a399b23dbe1013dc96244079e3d
guieenoutis <guieenoutis@gmail.com>
2018-05-18 15:58:01 +0900

    proceeding

--- src/migration/1526376251374-BalanceRefactoring.ts
+++ src/migration/1526376251374-BalanceRefactoring.ts
@@ -1,6 +1,6 @@
 // TODO:: NOT COMPLETED YET
 
-import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm'
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 
 export class BalanceRefactoring1526354141714 implements MigrationInterface {
   public async up (queryRunner: QueryRunner): Promise<any> {
@@ -9,81 +9,83 @@ export class BalanceRefactoring1526354141714 implements MigrationInterface {
       columns: [
         {
           name: 'id',
-          type: 'string',
+          type: 'uuid',
           isPrimary: true,
-          generationStrategy: 'uuid',
-          isGenerated: true
+          isGenerated: true,
+          generationStrategy: 'uuid'
         },
         {
           name: 'uniqueName',
           type: 'varchar',
-          isNullable: false,
-          isUnique: true
+          isUnique: true,
+          length: '255'
         },
         {
           name: 'amount',
-          type: 'int',
-          isNullable: false
-        },
-        {
-          name: 'balanceTransaction',
-          type: 'string'
-        }
-      ],
-      uniques: [
-        {
-          columnNames: ['uniqueName']
-        }
-      ],
-      checks: [
-        {
-          columnNames: ['amount'],
-          expression: `"amount" > 0`
-        }
-      ]
-    }), true)
-
-    await queryRunner.createTable(new Table({
-      name: 'transactionLogs',
-      columns: [
-        {
-          name: 'id',
-          type: 'string',
-          isPrimary: true
-        },
-        {
-          name: 'sender',
-          type: 'int'
-        },
-        {
-          name: 'receiver',
           type: 'int'
         },
         {
           name: 'createdDate',
-          type: 'date'
-        },
-        {
-          name: 'balance',
-          type: 'int'
+          type: 'date',
+          isGenerated: true
         }
       ]
-    }), true)
-
-    await queryRunner.createForeignKey('transactionLogs', new TableForeignKey({
-      columnNames: ['balance'],
-      referencedColumnNames: ['id'],
-      referencedTableName: 'balanceId',
-      onDelete: 'CASCADE'
+      // uniques: [
+      //   {
+      //     columnNames: ['uniqueName']
+      //   }
+      // ],
+      // checks: [
+      //   {
+      //     columnNames: ['amount'],
+      //     expression: `"amount" > 0`
+      //   }
+      // ]
     }))
+
+    // await queryRunner.createTable(new Table({
+    //   name: 'transactionLogs',
+    //   columns: [
+    //     {
+    //       name: 'id',
+    //       type: 'string',
+    //       isPrimary: true,
+    //       generationStrategy: 'uuid',
+    //       isGenerated: true
+    //     },
+    //     {
+    //       name: 'sender',
+    //       type: 'varchar',
+    //       isNullable: false,
+    //       length: '255'
+    //     },
+    //     {
+    //       name: 'receiver',
+    //       type: 'varchar',
+    //       isNullable: false,
+    //       length: '255'
+    //     },
+    //     {
+    //       name: 'createdDate',
+    //       type: 'date'
+    //     }
+    //   ]
+    // }), true)
+
+    // await queryRunner.createForeignKey('transactionLogs', new TableForeignKey({
+    //   columnNames: ['balance'],
+    //   referencedColumnNames: ['id'],
+    //   referencedTableName: 'balanceId',
+    //   onDelete: 'CASCADE'
+    // }))
   }
 
   public async down (queryRunner: QueryRunner): Promise<any> {
-    const table = await queryRunner.getTable('balance') as Table
-    const foreignKey = table.foreignKeys.find(fk => fk.columnNames.indexOf('balanceId') !== -1) as TableForeignKey
-    await queryRunner.dropForeignKey('balance', foreignKey)
-    await queryRunner.dropColumn('balance', 'balanceId')
-    await queryRunner.dropTable('transactionLogs')
+    // const table = await queryRunner.getTable('balance') as Table
+    // const foreignKey = table.foreignKeys.find(fk => fk.columnNames.indexOf('balanceId') !== -1) as TableForeignKey
+    // await queryRunner.dropForeignKey('balance', foreignKey)
+    // await queryRunner.dropColumn('balance', 'balanceId')
+    // await queryRunner.dropTable('transactionLogs')
     await queryRunner.dropTable('balance')
   }
 
