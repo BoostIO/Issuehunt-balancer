57d5ac764c04c0fc7950b6ddaec842adb99d369e
guieenoutis <guieenoutis@gmail.com>
2018-05-22 16:55:34 +0900

    edit to use HttpError of `routing-controllers`

--- src/middlewares/NotRegisteredRoute.ts
+++ src/middlewares/NotRegisteredRoute.ts
@@ -1,12 +1,14 @@
 import { NextFunction, Request, Response } from 'express'
-import { Middleware, ExpressMiddlewareInterface } from 'routing-controllers'
+import { Middleware, ExpressMiddlewareInterface, HttpError } from 'routing-controllers'
 
 @Middleware({ type: 'after' })
 export class NotRegisteredRoute implements ExpressMiddlewareInterface {
 
   public use (req: Request, res: Response, next?: NextFunction): void {
     if (!res.headersSent) {
-      throw new Error('API does not exist')
+      const e = new HttpError(404, 'API does not exist')
+      e.stack = null
+      throw e
     }
   }
 
