b3c6be33fbad2a3a7e63acfd2ad3897db396a5a6
guieenoutis <guieenoutis@gmail.com>
2018-05-24 16:49:12 +0900

    Remove table rather not drop database on each test

--- src/specs/dbLib.ts
+++ src/specs/dbLib.ts
@@ -1,6 +1,6 @@
 import { createTypeormConn } from '../lib/database/createTypeormConn'
 import configuration from '../lib/configuration'
-import { getConnection } from 'typeorm'
+import { getConnection, getRepository } from 'typeorm'
 
 const dbLib = {
   async connectDB () {
@@ -8,6 +8,21 @@ const dbLib = {
 
     return new Promise(async (resolve, reject) => {
       await createTypeormConn()
+      resolve()
+    })
+  },
+
+  async initializeEntityID () {
+    if (configuration.nodeEnv !== 'test') throw new Error('You can drop db on test mode only')
+
+    return new Promise(async (resolve, reject) => {
+      const entities = ['Balance', 'Log']
+      for (let i = 0; i < entities.length; i++) {
+        const allEntities = await getRepository(entities[i]).find({})
+        await getRepository(entities[i]).remove(allEntities)
+        await getRepository(entities[i]).query(`ALTER SEQUENCE ${entities[i]}_id_seq RESTART WITH 1`)
+        // TRUNCATE TABLE ${entities[i]} RESTART IDENTITY --> Somewhat reasons, it occurs error !
+      }
       resolve()
     })
   },
