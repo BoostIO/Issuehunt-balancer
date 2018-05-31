d54b7103309a7f882fad435cfe98d8b2fa5f6273
guieenoutis <guieenoutis@gmail.com>
2018-05-31 10:43:36 +0900

    delete unused properties on types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,18 +1,12 @@
-import { Log } from '../entity/Log'
-import { Balance } from '../entity/Balance'
-
 export interface LogBodyInterface {
   id?: number
   sender: string
   receiver: string
   amount: number | string
-  createdDate?: Date
-  balanceTransactions?: Balance[]
 }
 
 export interface BalanceBodyInterface {
   id?: number
   uniqueName: string
   amount: number | string
-  balanceTransaction?: Log
 }
