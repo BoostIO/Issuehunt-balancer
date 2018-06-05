a81f78e8a4ecac1b21e597a606c787b70f6d62c0
Junyoung Choi <fluke8259@gmail.com>
2018-06-06 03:29:56 +0900

    Add Transfer entity

---
+++ src/entities/Transfer.ts
@@ -0,0 +1,21 @@
+import { Entity, Column, BaseEntity, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm'
+
+@Entity('transfers')
+class Transfer extends BaseEntity {
+  @PrimaryGeneratedColumn('increment')
+  id!: string
+
+  @Column('bigint')
+  senderId!: string
+
+  @Column('bigint')
+  receiverId!: string
+
+  @Column('bigint')
+  amount!: string
+
+  @CreateDateColumn()
+  createdDate!: Date
+}
+
+export default Transfer
