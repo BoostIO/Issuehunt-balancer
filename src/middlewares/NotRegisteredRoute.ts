1dc1666be55f80a987e2e1d17276db3de42c02c2
Junyoung Choi <fluke8259@gmail.com>
2018-06-05 18:25:30 +0900

    Refactor NotRegisteredRoute

--- src/middlewares/NotRegisteredRoute.ts
+++ src/middlewares/NotRegisteredRoute.ts
@@ -1,13 +1,12 @@
 import { NextFunction, Request, Response } from 'express'
-import { Middleware, ExpressMiddlewareInterface, HttpError } from 'routing-controllers'
+import { Middleware, ExpressMiddlewareInterface } from 'routing-controllers'
+import { NotFoundError }from '../lib/errors'
 
 @Middleware({ type: 'after' })
 export class NotRegisteredRoute implements ExpressMiddlewareInterface {
-
   public use (req: Request, res: Response, next?: NextFunction): any {
     if (!res.headersSent) {
-      throw new HttpError(404, 'API does not exist')
+      throw new NotFoundError('The given route does not exist.')
     }
   }
-
 }
--- src/specs/nnotRegisteredRoute.spec.ts
+++ src/specs/nnotRegisteredRoute.spec.ts
@@ -1,19 +1,17 @@
 import chai from 'chai'
-import db from './lib/db'
 import app from '../app'
 
 describe('Access to not registered routes', () => {
-  beforeEach(db.connectDB)
-  afterEach(db.initializeEntityID)
-
   it('does not registered route', async () => {
     const result = await chai.request(app)
       .get('/notRegisteredRoute')
 
-    expect(result.status).toEqual(404)
     expect(JSON.parse(result.text))
       .toEqual({
-        'message': 'API does not exist', 'name': 'Error', 'status': 404
+        message: 'The given route does not exist.',
+        name: 'Error',
+        status: 404
       })
+    expect(result.status).toEqual(404)
   })
 })
